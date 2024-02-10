e60e96b932855e10336fec3e33b5ebfbfc860bf3
Sandesh <112424645+Sandy177000@users.noreply.github.com>
2024-02-10 19:11:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,27 +1,34 @@
-//complete this code
 class Person {
-	let name = "";
-	let age = "";
 	
 	constructor(name,age)
 	{
 		this.name  = name;
 		this.age = age;
 	}
+
+	getname(){
+		return this.name;
+	}
+
+	setname(name){
+		this.name = name;
+	}
 }
 
 class Student extends Person {
 
-	function study(name){
-		console.log(name + " is studying");
+	study(){
+		console.log(this.name + " is studying");
 	}
 }
 
 class Teacher extends Person {
-	function teach(name){
-		console.log(name + " is teaching");
+	teach(){
+		console.log(this.name + " is teaching");
 	}
 }
+
+
 
 // Do not change the code below this line
 window.Person = Person;
